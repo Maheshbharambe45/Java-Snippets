044611a984951fe05ea1287def66245d710ad053
Mahesh Bharambe <bharambemahesh7@gmail.com>
2025-01-11 19:43:09 +0530

    files added

--- elseIf.java
+++ elseIf.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Practise {
+public class elseIf {
     public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     System.out.println("enter the number");
