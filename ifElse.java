e73e91ac718e108728b6950e2fb5303e14331aef
Mahesh Bharambe <bharambemahesh7@gmail.com>
2025-01-11 19:08:06 +0530

    ifElse.java added

--- ifElse.java
+++ ifElse.java
@@ -1,14 +1,15 @@
+import java.util.*;
+
 public class ifElse {
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    System.out.println("Please enter your Age");
-    int age = sc.nextInt();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter your Age");
+        int age = sc.nextInt();
 
-    if(age>=18){
-        System.out.println("Yey , you are eligible for vote");
-    }
-    else{
-        System.out.println(" Oops , your age is "+age+" please come after 18 ");
-    }
+        if (age >= 18) {
+            System.out.println("Yey , you are eligible for vote");
+        } else {
+            System.out.println(" Oops , your age is " + age + " please come after 18 ");
+        }
     }
 }
